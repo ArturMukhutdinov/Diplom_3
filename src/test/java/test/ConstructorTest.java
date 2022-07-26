6defcc35359054a3dc81e697b1829a48fe273d53
ArturMukhutdinov <muhutdinov.artur@yandex.ru>
2022-07-26 17:11:38 +0300

    1 commit

---
+++ src/test/java/test/ConstructorTest.java
@@ -0,0 +1,56 @@
+package test;
+
+import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.WebDriverRunner;
+import io.qameta.allure.Step;
+import org.junit.Test;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+public class ConstructorTest extends BaseTest {
+
+
+
+    @Test
+    public void testNavigationToBuns() {
+        waitForJsToLoad();
+        mainPage.clickSauces();
+        mainPage.clickIngredients();
+
+        mainPage.clickBuns();
+        waitUntilCategoryIsActive(mainPage.getBunsButton());
+
+    }
+
+    @Test
+    public void testNavigationToSauces() {
+        waitForJsToLoad();
+        mainPage.clickIngredients();
+
+        mainPage.clickSauces();
+        waitUntilCategoryIsActive(mainPage.getSaucesButton());
+    }
+
+    @Test
+    public void testNavigationToIngredients() {
+
+        waitForJsToLoad();
+        mainPage.clickSauces();
+
+        mainPage.clickIngredients();
+        waitUntilCategoryIsActive(mainPage.getIngredientsButton());
+    }
+
+    @Step("Wait until elements parent has 'current' class")
+    private void waitUntilCategoryIsActive(SelenideElement element) {
+        new WebDriverWait(WebDriverRunner.getWebDriver(), 1L)
+                .until(ExpectedConditions.attributeContains(element.parent(), "class", "tab_tab_type_current__2BEPc"));
+    }
+
+    private void waitForJsToLoad() {
+
+        WebDriverRunner.getWebDriver().manage().timeouts().implicitlyWait(1L, TimeUnit.SECONDS);
+    }
+}
